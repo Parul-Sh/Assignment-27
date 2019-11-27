0bc2f3800ce53ec64454e4e1f8e50e1f2ac293b4
HP <HP@LAPTOP-DKRISG2B>
2019-11-27 18:30:13 +0530

    Commit

---
+++ PATTERNN/src/My.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class My {
+
+	public static void main(String[] args) {
+		int i,j, k;
+		for(i=1;i<=4;i++)
+		{
+			for(j=3;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(k=1;k<=i;k++)
+			{
+				System.out.print("* ");
+			}
+			System.out.print("\n");
+		}
+		for(i=1;i<=5;i++)
+		{
+			System.out.print("* ");
+		}
+		System.out.print("\n");
+		for(i=4;i>=1;i--)
+		{
+			for(j=4;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(k=1;k<=i;k++)
+			{
+				System.out.print("* ");
+			} 
+			System.out.print("\n");
+		}
+		
+        
+	}
+
+}
