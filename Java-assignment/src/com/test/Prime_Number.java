487ed89bad740ee4296885ae2aca083a8116017c
Parul-Sh <57749861+Parul-Sh@users.noreply.github.com>
2019-12-03 16:25:08 +0530

    Update Prime_Number.java

--- Java-assignment/src/com/test/Prime_Number.java
+++ Java-assignment/src/com/test/Prime_Number.java
@@ -6,16 +6,16 @@ public class Prime_Number {
 		
 		int count=0;
 		System.out.println("Prime numbers are:");
-		for(int n=2;n<=100;n++) {
-        for(int i=2;i<(n/2);i++)
+		for(int n=2;n<=100;n++) { // Iteration from 2 to 100 for finding prime numbers between them
+        for(int i=2;i<(n/2);i++)//Iteration from 2 to n/2
         {
         	if((n%i)==0)
         	{
-        		count++;
+        		count++;//count=count+1
         	
         	}
         }
-         
+         //If count=0 means number is prime else not
         if(count==0)
         { System.out.println(n);
            
