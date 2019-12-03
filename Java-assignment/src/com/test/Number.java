9b80f6a940247f351a10ee9825b0b2a7d7280416
Parul-Sh <57749861+Parul-Sh@users.noreply.github.com>
2019-12-03 16:16:19 +0530

    Update Number.java

--- Java-assignment/src/com/test/Number.java
+++ Java-assignment/src/com/test/Number.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 public class Number {
 	public static void my() {
 	Scanner scan = new Scanner(System.in);
-	int a= scan.nextInt();
-	int b= scan.nextInt();
-	int c= scan.nextInt();
+	int a= scan.nextInt();//Scanning first number
+	int b= scan.nextInt();//Scanning second number
+	int c= scan.nextInt();//Scanning third number
 	System.out.println("Greatest number is:");
 	 if(a>=b)
 	 {
 		 if(a>=c)
-		 {System.out.println(a);}
+		 {System.out.println(a);//Printing a as a is greatest}
 		 else
-		 {System.out.println(c);}
+		 {System.out.println(c);//Printing  c as c is greatest}
 	 }
 	 else
 	 {
 		 if(b>=c)
-		 {System.out.println(b);}
+		 {System.out.println(b);//Printing b as b is greatest
 		 else
-			 System.out.println(c);
+			 System.out.println(c);//Printing c as c is greatest
 	 }
 
 	}
