5d119861d6ec63c97b55f7009d9f42a236032748
Parul-Sh <57749861+Parul-Sh@users.noreply.github.com>
2019-12-03 16:21:49 +0530

    Update Prime.java

--- Java-assignment/src/com/test/Prime.java
+++ Java-assignment/src/com/test/Prime.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class Prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		Scanner scan = new Scanner(System.in);//Initializing the scanner object 
+		int n = scan.nextInt();//Scanning the number to check if it is prime or not
 		int count=0;
-        for(int i=2;i<(n/2);i++)
+        for(int i=2;i<(n/2);i++)//Iterate from 2 to n/2
         {
         	if((n%i)==0)
         	{
         		count++;
         	}
         }
+		// if count =  0 means the number is prime else not
         if(count == 0)
         {
         	System.out.println("Prime Number");
