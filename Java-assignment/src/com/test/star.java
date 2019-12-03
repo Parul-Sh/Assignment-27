fd21d3cb146f0647eb5960d2665e7627cd387424
Parul-Sh <57749861+Parul-Sh@users.noreply.github.com>
2019-12-03 16:30:48 +0530

    Update star.java

--- Java-assignment/src/com/test/star.java
+++ Java-assignment/src/com/test/star.java
@@ -3,8 +3,8 @@ package com.test;
 public class star {
 
 	public static void main(String[] args) {
-	for(int j=1;j<=4;j++) {
-		for(int i=1;i<=j;i++)
+	for(int j=1;j<=4;j++) {  //Iteration from 1 to 4 for rows
+		for(int i=1;i<=j;i++) // Iteration from 1 to j for columns
 		{
 			System.out.print("*");
 		}
